d9c3230e7a9bc388e61d5a82bc731db73f91fef4
James Thompson <thompson.tomo@outlook.com>
2026-07-06 13:36:53 +1000

    Add Prettier

--- packages/frontmatter/src/index.js
+++ packages/frontmatter/src/index.js
@@ -1,4 +1,4 @@
-const yaml = require('js-yaml');
+const yaml = require("js-yaml");
 
 /**
  * @param {import("@textlint/types").TextlintRuleContext} context
@@ -6,61 +6,69 @@ const yaml = require('js-yaml');
  * @returns {import("@textlint/types").TextlintRuleCreator}
  */
 export default function (context, options = {}) {
-    const { Syntax, RuleError, report, locator } = context;
-    const matchingTitles = options["titles-must-match"] ?? true;
-    const propertyOrder = options["property-order"] ?? [];
-    var frontmatter;
-    var titleMatched;
-    return {
-        ['Yaml'](node) { // "Yaml" node
-            const text = node.value; // Get text
-            frontmatter = yaml.load(text);
-            if(propertyOrder.length === 0) {
-                return;
-            }
-            const actual = Object.keys(frontmatter);
-            for(var i = 0; i < propertyOrder.length; i++) {
-                var position = actual.indexOf(propertyOrder[i]);
-                if (position === -1) {
-                    const ruleError = new RuleError(`Missing required property: ${propertyOrder[i]}.`);
-                    report(node, ruleError);
-                }
-                else if (position !== i) {
-                    const ruleError = new RuleError(`Property ${propertyOrder[i]} is out of order. Expected position: ${i}, Actual position: ${position}.`);
-                    report(node, ruleError);
-                }
-            }
-        },
-        ['Header'](node) { // "Header" node
-            if (node.depth !== 1) {
-                return;
-            }
-            const text = node.children[0]?.raw; // Get text
-            if (!matchingTitles) {
-                return;
-            }
-            if (text === frontmatter?.title) {
-                titleMatched = true;
-                return;
-            }
-            else if (frontmatter?.title === undefined) {
-                const ruleError = new RuleError("No FrontMatter Title found to match to.");
-                titleMatched = false;
-                report(node, ruleError);
-            }
-            else {
-                const ruleError = new RuleError("Header does not match FrontMatter title.");
-                titleMatched = false;
-                report(node, ruleError);
-            }
-        },
-        [Syntax.DocumentExit](node) {
-            if ( matchingTitles && titleMatched === undefined && frontmatter?.title !== undefined) {
-                report(
-                    node,
-                    new RuleError("No Header matches FrontMatter title.")
-                );
-            }
+  const { Syntax, RuleError, report, locator } = context;
+  const matchingTitles = options["titles-must-match"] ?? true;
+  const propertyOrder = options["property-order"] ?? [];
+  var frontmatter;
+  var titleMatched;
+  return {
+    ["Yaml"](node) {
+      // "Yaml" node
+      const text = node.value; // Get text
+      frontmatter = yaml.load(text);
+      if (propertyOrder.length === 0) {
+        return;
+      }
+      const actual = Object.keys(frontmatter);
+      for (var i = 0; i < propertyOrder.length; i++) {
+        var position = actual.indexOf(propertyOrder[i]);
+        if (position === -1) {
+          const ruleError = new RuleError(
+            `Missing required property: ${propertyOrder[i]}.`,
+          );
+          report(node, ruleError);
+        } else if (position !== i) {
+          const ruleError = new RuleError(
+            `Property ${propertyOrder[i]} is out of order. Expected position: ${i}, Actual position: ${position}.`,
+          );
+          report(node, ruleError);
         }
-    }
-};
+      }
+    },
+    ["Header"](node) {
+      // "Header" node
+      if (node.depth !== 1) {
+        return;
+      }
+      const text = node.children[0]?.raw; // Get text
+      if (!matchingTitles) {
+        return;
+      }
+      if (text === frontmatter?.title) {
+        titleMatched = true;
+        return;
+      } else if (frontmatter?.title === undefined) {
+        const ruleError = new RuleError(
+          "No FrontMatter Title found to match to.",
+        );
+        titleMatched = false;
+        report(node, ruleError);
+      } else {
+        const ruleError = new RuleError(
+          "Header does not match FrontMatter title.",
+        );
+        titleMatched = false;
+        report(node, ruleError);
+      }
+    },
+    [Syntax.DocumentExit](node) {
+      if (
+        matchingTitles &&
+        titleMatched === undefined &&
+        frontmatter?.title !== undefined
+      ) {
+        report(node, new RuleError("No Header matches FrontMatter title."));
+      }
+    },
+  };
+}
