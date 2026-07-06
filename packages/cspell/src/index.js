1e6dada00949ba43631194de680058a30a022195
James Thompson <thompson.tomo@outlook.com>
2026-07-06 17:25:48 +1000

    Update index.js

--- packages/cspell/src/index.js
+++ packages/cspell/src/index.js
@@ -1,26 +1,27 @@
+const { resolve } = require("node:path");
+const { pathToFileURL } = require("node:url");
+const { spellCheckDocument } = require("cspell-lib");
+
 /**
  * @param {import("@textlint/types").TextlintRuleContext} context
  * @param {import("@textlint/types").TextlintRuleOptions<{ allows?: string[]}>} options
  * @returns {import("@textlint/types").TextlintRuleCreator}
  */
 export default function (context, options = {}) {
-    const { Syntax, RuleError, report, getSource, locator } = context;
-    const allows = options.allows ?? [];
-    return {
-        [Syntax.Str](node) { // "Str" node
-            const text = getSource(node); // Get text
-            if (allows.some(allow => text.includes(allow))) {
-                return;
-            }
-            const matches = text.matchAll(/bugs/g);
-            for (const match of matches) {
-                const index = match.index ?? 0;
-                const matchRange = [index, index + match[0].length];
-                const ruleError = new RuleError("Found bugs.", {
-                    padding: locator.range(matchRange)
-                });
-                report(node, ruleError);
-            }
-        }
+  const { Syntax, RuleError, report, getSource, locator } = context;
+  const allows = options.allows ?? [];
+  return {
+    async [Syntax.DocumentEnd](node) { // "Str" node
+      const uri = pathToFileURL(resolve(filename)).toString();
+      const result = await spellCheckDocument(
+        { uri },
+        { generateSuggestions: true, noConfigSearch: true },
+        { words: customWords, suggestionsTimeout: 2000 }
+      );
+      console.log(result.issues);
+      for(var issue in result.issues){
+        const ruleError = new RuleError(issue.text);
+        report(node, ruleError);
+      }
     }
 };
